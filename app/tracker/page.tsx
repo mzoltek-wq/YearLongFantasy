e516eb7f707176e9536dafe75266c81b7a4eaae3
zoltek <mzoltek@gmail.com>
2026-08-15 22:40:00 -0400

    Redirect tracker to league view

--- app/tracker/page.tsx
+++ app/tracker/page.tsx
@@ -1,10 +1,7 @@
-import { DraftBoardClient } from "@/components/draft/draft-board-client";
-import { getLeagueSnapshot } from "@/lib/draft/service";
+import { redirect } from "next/navigation";
 
 export const dynamic = "force-dynamic";
 
-export default async function TrackerPage() {
-  const snapshot = await getLeagueSnapshot();
-
-  return <DraftBoardClient initialSnapshot={snapshot} mode="tracker" />;
+export default function TrackerPage() {
+  redirect("/league-view");
 }
